ebb867dd058dcb89ec6c38ac751ce0a8d1921383
arunarivanandam <arunarivanandam@gmail.com>
2024-02-21 21:33:19 +0530

    request logging

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -6,11 +6,12 @@ func (app *application) routes() http.Handler {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
+
 	mux.HandleFunc("/snippet", app.showSnippet)
 	mux.HandleFunc("/snippet/create", app.createSnippet)
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	return secureHeaders(mux)
+	return app.logRequest(secureHeaders(mux))
 }
