3920c930fe6bdf3abc6787d9b9409963cdd5592e
arunarivanandam <arunarivanandam@gmail.com>
2024-02-21 05:30:03 +0530

    template actions and functions

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,5 +3,6 @@ package main
 import "github.com/ArunArivanandam/snippet/pkg/models"
 
 type templateData struct {
-	Snippet *models.Snippet
+	Snippet  *models.Snippet
+	Snippets []*models.Snippet
 }
