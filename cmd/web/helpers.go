5834242119afd88ce26f032f929c639019f23b77
arunarivanandam <arunarivanandam@gmail.com>
2024-02-21 06:59:27 +0530

    caching templates

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -8,7 +8,7 @@ import (
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
-	app.errorLog.Output(2,trace)
+	app.errorLog.Output(2, trace)
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
@@ -18,4 +18,16 @@ func (app *application) clientError(w http.ResponseWriter, status int) {
 
 func (app *application) notFound(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
+}
+
+func (app *application) render(w http.ResponseWriter, r *http.Request, name string, td *templateData) {
+	ts, ok := app.templateCache[name]
+	if !ok {
+		app.serverError(w, fmt.Errorf("The template %s does not exist", name))
+		return
+	}
+	err := ts.Execute(w, td)
+	if err != nil {
+		app.serverError(w, err)
+	}
 }
